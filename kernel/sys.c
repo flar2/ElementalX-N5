f2c8cf1d7f212a02743c4729e222cd9ad5cb9c6f
Lance Poore <linuxsociety@gmail.com>
2012-08-12 15:25:06 -0500

    SCHEDULER: Autogroup patch group by current user android UID instead of task ID
    Signed-off-by: flar2 <asegaert@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -652,6 +652,7 @@ static int set_user(struct cred *new)
 
 	free_uid(new->user);
 	new->user = new_user;
+	sched_autogroup_create_attach(current);
 	return 0;
 }
 
@@ -1161,7 +1162,7 @@ out:
 	write_unlock_irq(&tasklist_lock);
 	if (err > 0) {
 		proc_sid_connector(group_leader);
-		sched_autogroup_create_attach(group_leader);
+		
 	}
 	return err;
 }
--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -366,7 +366,7 @@ static struct ctl_table kern_table[] = {
 		.data		= &sysctl_sched_autogroup_enabled,
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= proc_dointvec,
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
