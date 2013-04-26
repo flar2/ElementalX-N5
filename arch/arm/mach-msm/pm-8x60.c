1b87856d245432612d070cb23303bb254bcb3d92
Mahesh Sivasubramanian <msivasub@codeaurora.org>
2013-04-26 11:40:27 -0600

    msm: mpm: Use clock API to prevent XO shutdown
    When the active interrupts cannot be monitored by MPM during system
    sleep modes, use linux clock APIs to vote against XO shutdown. Earlier
    mpm driver relies on lpm resoures to overwrite the clock votes based on
    enabled interrupts. This change decouples the MPM driver from the
    lpm resources and thereby removing the dependency on RPM driver.
    
    Change-Id: Ica5409ebb6744998303d84867f9b6d0243664b7e
    Signed-off-by: Mahesh Sivasubramanian <msivasub@codeaurora.org>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- arch/arm/mach-msm/pm-8x60.c
+++ arch/arm/mach-msm/pm-8x60.c
@@ -42,6 +42,7 @@
 #include <mach/trace_msm_low_power.h>
 #include <mach/msm-krait-l2-accessors.h>
 #include <mach/msm_bus.h>
+#include <mach/mpm.h>
 #include <asm/cacheflush.h>
 #include <asm/hardware/gic.h>
 #include <asm/pgtable.h>
@@ -1200,12 +1201,14 @@ void msm_pm_set_sleep_ops(struct msm_pm_sleep_ops *ops)
 
 int msm_suspend_prepare(void)
 {
+	msm_mpm_suspend_prepare();
 	suspend_time = msm_pm_timer_enter_suspend(&suspend_period);
 	return 0;
 }
 
 void msm_suspend_wake(void)
 {
+	msm_mpm_suspend_wake();
 	if (suspend_power_collapsed) {
 		suspend_time = msm_pm_timer_exit_suspend(suspend_time,
 				suspend_period);
@@ -1221,6 +1224,8 @@ void msm_suspend_wake(void)
 static const struct platform_suspend_ops msm_pm_ops = {
 	.enter = msm_pm_enter,
 	.valid = suspend_valid_only_mem,
+	.prepare_late = msm_suspend_prepare,
+	.wake = msm_suspend_wake,
 };
 
 static int __devinit msm_pm_snoc_client_probe(struct platform_device *pdev)
