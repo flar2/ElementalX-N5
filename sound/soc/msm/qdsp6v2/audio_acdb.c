fe6e2f609d463c447b8c205a8a4a2ba1fc896c24
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2013-08-16 18:05:07 -0400

    ASoC: msm: qdsp6v2: Clear ION handle after freeing
    Once ion memory is freed ion handle should be set to
    NULL to avoid double free or any unintended access.
    This change clears the ION handle after freeing up
    the memory.
    
    Change-Id: I37d76fe2641c111fa4376eff8f7e45f6795414ca
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- sound/soc/msm/qdsp6v2/audio_acdb.c
+++ sound/soc/msm/qdsp6v2/audio_acdb.c
@@ -1213,6 +1213,8 @@ static int deregister_memory(void)
 			acdb_data.col_data[i] = NULL;
 		}
 		msm_audio_ion_free(acdb_data.ion_client, acdb_data.ion_handle);
+		acdb_data.ion_client = NULL;
+		acdb_data.ion_handle = NULL;
 		mutex_unlock(&acdb_data.acdb_mutex);
 	}
 	mutex_unlock(&acdb_data.acdb_mutex);
@@ -1263,8 +1265,6 @@ static int register_memory(void)
 
 	return result;
 err_ion_handle:
-	msm_audio_ion_free(acdb_data.ion_client, acdb_data.ion_handle);
-
 	atomic64_set(&acdb_data.mem_len, 0);
 	mutex_unlock(&acdb_data.acdb_mutex);
 	return result;
