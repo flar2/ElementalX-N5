a1613cccd47135d8ec7efa2e13076b0c90076b88
flar2 <asegaert@gmail.com>
2013-11-18 21:39:40 -0500

    Basic color preset
    I scooped the payload value from @fransiscofranco https://github.com/franciscofranco/hammerhead/commit/6747c5003dba8fda142b7a4041db3033dda6d036
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -30,6 +30,11 @@
 
 #define DT_CMD_HDR 6
 
+//Basic color preset
+int color_preset = 0;
+module_param(color_preset, int, 0755);
+
+
 static bool mdss_panel_flip_ud = false;
 static int mdss_panel_id = PANEL_QCOM;
 
@@ -287,6 +292,12 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	mipi  = &pdata->panel_info.mipi;
 
 	pr_debug("%s: ctrl=%p ndx=%d\n", __func__, ctrl, ctrl->ndx);
+
+//Basic color preset 
+	if (color_preset == 1)
+		ctrl->on_cmds.cmds[1].payload[0] = 0x77;
+	else
+		ctrl->on_cmds.cmds[1].payload[0] = 0xFF;
 
 	if (ctrl->on_cmds.cmd_cnt)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->on_cmds);
