a7d9c3606096b5939bd952d4ba677b345785b725
flar2 <asegaert@gmail.com>
2013-11-26 13:41:48 -0500

    [s2w/dt2w] Prevent touches during phone calls

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -29,6 +29,9 @@
 #include "audio_acdb.h"
 #include "q6voice.h"
 
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#include <linux/input/prevent_sleep.h>
+#endif
 
 #define TIMEOUT_MS 500
 
@@ -40,6 +43,10 @@
 #define CVP_CAL_SIZE 245760
 /* CVS CAL Size: 49152 = 48 * 1024 */
 #define CVS_CAL_SIZE 49152
+
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+bool in_phone_call = false;
+#endif
 
 enum {
 	VOC_TOKEN_NONE,
@@ -4323,6 +4330,9 @@ int voc_end_voice_call(uint32_t session_id)
 
 		v->voc_state = VOC_RELEASE;
 	}
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+	in_phone_call = false;
+#endif
 	mutex_unlock(&v->lock);
 	return ret;
 }
@@ -4529,6 +4539,10 @@ int voc_start_voice_call(uint32_t session_id)
 		ret = -EINVAL;
 		goto fail;
 	}
+#ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+	in_phone_call = true;
+#endif
+
 fail:
 	mutex_unlock(&v->lock);
 	return ret;
