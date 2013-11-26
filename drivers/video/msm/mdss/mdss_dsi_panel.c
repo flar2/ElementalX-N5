ac86eaf7f0197fc3b0563e4969e015ff12b9908e
flar2 <asegaert@gmail.com>
2013-11-26 13:41:48 -0500

    Prevent touches during phone calls
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -47,6 +47,7 @@ DEFINE_LED_TRIGGER(bl_led_trigger);
 extern void lm3630_lcd_backlight_set_level(int level);
 #endif
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
+#include <linux/input/prevent_sleep.h>
 #ifdef CONFIG_TOUCHSCREEN_SWEEP2WAKE
 #include <linux/input/sweep2wake.h>
 #endif
@@ -200,6 +201,8 @@ void mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable)
 #if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
+	if (prevent_sleep && in_phone_call)
+		prevent_sleep = false;
 #endif
 #ifdef CONFIG_PWRKEY_SUSPEND
 	if (pwrkey_pressed)
@@ -365,6 +368,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 #if defined(CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE)
 	prevent_sleep = prevent_sleep || (dt2w_switch > 0);
 #endif
+	if (prevent_sleep && in_phone_call)
+		prevent_sleep = false;
 #endif
 #ifdef CONFIG_PWRKEY_SUSPEND
 	if (pwrkey_pressed)
