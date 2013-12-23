fb4cb88cf705bd97ef4bf7126bc53e084c24bbcb
Matthew Qin <yqin@codeaurora.org>
2013-12-23 20:07:48 +0800

    qpnp-rtc: clear alarm register when rtc irq is disabled
    The rtc alarm register should be cleared when the rtc irq is
    disabled
    
    Change-Id: I97a8bf989ff610093240a6b308a297702da6cb89
    Signed-off-by: Xiaocheng Li <lix@codeaurora.org>
    Signed-off-by: Matthew Qin <yqin@codeaurora.org>

--- include/linux/alarmtimer.h
+++ include/linux/alarmtimer.h
@@ -46,7 +46,7 @@ void alarm_init(struct alarm *alarm, enum alarmtimer_type type,
 void alarm_start(struct alarm *alarm, ktime_t start);
 int alarm_try_to_cancel(struct alarm *alarm);
 int alarm_cancel(struct alarm *alarm);
-void set_power_on_alarm(long secs);
+void set_power_on_alarm(long secs, bool enable);
 
 u64 alarm_forward(struct alarm *alarm, ktime_t now, ktime_t interval);
 
