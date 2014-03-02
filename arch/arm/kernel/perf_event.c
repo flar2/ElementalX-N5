86a8c5031ef6d1fd459f7ca8dfed5dcc15ba01ef
flar2 <asegaert@gmail.com>
2014-03-02 16:02:04 -0500

    Revert "ARM: 7810/1: perf: Fix array out of bounds access in armpmu_map_hw_event()"
    This reverts commit e4e22103505ae0fe0a4c0cc33f9bafa4df6ea717.

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -119,7 +119,7 @@ armpmu_map_event(const unsigned (*event_map)[PERF_COUNT_HW_MAX], u64 config)
 	int mapping;
 
 	if (config >= PERF_COUNT_HW_MAX)
-		return -EINVAL;
+		return -ENOENT;
 
 	mapping = (*event_map)[config];
 	return mapping == HW_OP_UNSUPPORTED ? -ENOENT : mapping;
