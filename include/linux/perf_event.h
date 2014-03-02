8d86207689e22580acedf6e35146ef4cba9a53dc
flar2 <asegaert@gmail.com>
2014-03-02 16:02:11 -0500

    Revert "msm: perf: clean up duplicate constraint events"
    This reverts commit 3fccc78130891ba534bae8b9a37773d1408699ba.

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -254,9 +254,8 @@ struct perf_event_attr {
 
 				exclude_host   :  1, /* don't count in host   */
 				exclude_guest  :  1, /* don't count in guest  */
-				constraint_duplicate : 1,
 
-				__reserved_1   : 42;
+				__reserved_1   : 43;
 
 	union {
 		__u32		wakeup_events;	  /* wakeup every n events */
