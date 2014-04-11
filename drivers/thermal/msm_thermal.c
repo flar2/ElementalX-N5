b61aefa5b2022df20c3b03626971645a3f1bafb3
Shiju Mathew <shijum@codeaurora.org>
2014-04-11 19:37:20 -0400

    msm: thermal: Update to speed up bootup time
    Update KTM driver to remove flush_scheduled_work that
    would introduce bootup delay. This patch replaces
    cancel_delayed_work with cancel_delayed_work_sync
    to cancel a delayed work.
    
    CRs-Fixed: 647439
    Change-Id: Ie81278ca3a7a58f8f46463f7149b512bd07cbcbc
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -906,8 +906,7 @@ static void __ref disable_msm_thermal(void)
 	int cpu = 0;
 
 	/* make sure check_temp is no longer running */
-	cancel_delayed_work(&check_temp_work);
-	flush_scheduled_work();
+	cancel_delayed_work_sync(&check_temp_work);
 
 	if (limited_max_freq == MSM_CPUFREQ_NO_LIMIT)
 		return;
