b2d88b204baef853aaa7248fc3bc0dabca4ed836
imoseyon <imoseyon@gmail.com>
2015-02-07 08:32:53 -0800

    power: skip sensor_ind wakeup source activation via sysfs
    * HACK ALERT!!
    
    Probably revert when future ROM has this wakelock fixed

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -16,6 +16,10 @@
 #include <linux/debugfs.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
+#include <linux/moduleparam.h>
+
+static bool enable_si_ws = true;
+module_param(enable_si_ws, bool, 0644);
 
 #include "power.h"
 
@@ -508,6 +512,10 @@ static void wakeup_source_deactivate(struct wakeup_source *ws)
 	if (ws->autosleep_enabled)
 		update_prevent_sleep_time(ws, now);
 
+	if (!enable_si_ws && !strcmp(ws->name, "sensor_ind")) {
+		pr_info("wakeup source sensor_ind activate skipped\n");
+		return;
+	}
 	/*
 	 * Increment the counter of registered wakeup events and decrement the
 	 * couter of wakeup events in progress simultaneously.
