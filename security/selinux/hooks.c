6c0924b1e867542aea0d7690d4fe44c1dd421877
flar2 <asegaert@gmail.com>
2015-11-08 15:17:01 -0500

    selinux: enforcing by default
    Signed-off-by: flar2 <asegaert@gmail.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -101,7 +101,7 @@ extern struct security_operations *security_ops;
 static atomic_t selinux_secmark_refcount = ATOMIC_INIT(0);
 
 #ifdef CONFIG_SECURITY_SELINUX_DEVELOP
-int selinux_enforcing;
+int selinux_enforcing = 1;
 
 static int __init enforcing_setup(char *str)
 {
