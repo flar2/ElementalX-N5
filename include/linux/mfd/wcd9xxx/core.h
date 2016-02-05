7b8454dd228ccd5e799aa4fe35f3b94fa32e99c5
flar2 <asegaert@gmail.com>
2016-02-04 21:52:55 -0500

    Revert "Revert "Sound Control: (Optional) work around for Nexus 4/5 audio issues""
    This reverts commit 06d6f83b35b502865e604d1620ee97d67d7fd9ef.

--- include/linux/mfd/wcd9xxx/core.h
+++ include/linux/mfd/wcd9xxx/core.h
@@ -223,6 +223,9 @@ struct wcd9xxx {
 };
 
 int wcd9xxx_reg_read(struct wcd9xxx *wcd9xxx, unsigned short reg);
+#ifdef CONFIG_SOUND_CONTROL_HAX_3_GPL
+int wcd9xxx_reg_read_safe(struct wcd9xxx *wcd9xxx, unsigned short reg);
+#endif
 int wcd9xxx_reg_write(struct wcd9xxx *wcd9xxx, unsigned short reg,
 		u8 val);
 int wcd9xxx_interface_reg_read(struct wcd9xxx *wcd9xxx, unsigned short reg);
