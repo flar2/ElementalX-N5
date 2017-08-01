aosp-mirror/kernel_msm

qdsp6v2: blacklist %p kptr_restrict
Bug: 31498403
Change-Id: Ie16da820fbbc1cc5815ef9e5a2566107d666a1bb

sound/soc/msm/qdsp6v2/q6lsm.c
@@ -80,7 +80,7 @@ static int q6lsm_callback(struct apr_client_data *data, void *priv)
 	uint32_t *payload;
 
 	if (!client || !data) {
-		pr_err("%s: client %p data %p\n",
+		pr_err("%s: client %pK data %pK\n",
 			__func__, client, data);
 		WARN_ON(1);
 		return -EINVAL;
@@ -711,7 +711,7 @@ static int q6lsm_memory_map_regions(struct lsm_client *client,
 	int rc;
 	int cmd_size = 0;
 
-	pr_debug("%s: dma_addr_p 0x%pa, dma_buf_sz %d, mmap_p 0x%p, session %d\n",
+	pr_debug("%s: dma_addr_p 0x%pa, dma_buf_sz %d, mmap_p 0x%pK, session %d\n",
 		__func__, &dma_addr_p, dma_buf_sz, mmap_p,
 		client->session);
 	if (CHECK_SESSION(client->session)) {
@@ -931,7 +931,7 @@ int q6lsm_snd_model_buf_alloc(struct lsm_client *client, size_t len)
 	size_t pad_zero = 0, total_mem = 0;
 
 	if (!client || len <= LSM_ALIGN_BOUNDARY) {
-		pr_err("%s: client %p len %zu\n",
+		pr_err("%s: client %pK len %zu\n",
 			__func__, client, len);
 		return rc;
 	}

flar2/ElementalX-N5

sound/soc/msm/qdsp6v2/q6lsm.c
@@ -516,7 +516,7 @@ static int q6lsm_memory_map_regions(struct lsm_client *client,
 	int rc;
 	int cmd_size = 0;
 
-	pr_debug("%s: dma_addr_p 0x%x, dma_buf_sz %d, mmap_p 0x%p, session %d\n",
+	pr_debug("%s: dma_addr_p 0x%x, dma_buf_sz %d, mmap_p 0x%pK, session %d\n",
 		 __func__, dma_addr_p, dma_buf_sz, mmap_p, client->session);
 
 	cmd_size = sizeof(struct avs_cmd_shared_mem_map_regions) +